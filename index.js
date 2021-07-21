eaf4450474dad7b4c5cf2865e521e2e2900580e3
Andrejus Timko <timcostudios@gmail.com>
2021-07-21 11:29:42 +0300

    express

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 // start of back end
 require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const app = express();
+
+const PORT = 4000;
+
+// prisijungimas prie duomenu bazes
+// mongoose
+//   .connect(process.env.MONGO_CONNECT_STIRNG, {
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then((result) => {
+//     console.log('Conneced to my Mongoose!');
+//   })
+//   .catch((err) => console.error(err.message));
+
+// const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(3000);
