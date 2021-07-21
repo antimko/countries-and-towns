22d985848d728309da5b8f7c555299c985971adc
Andrejus Timko <timcostudios@gmail.com>
2021-07-21 11:41:22 +0300

    .env changed MongoDb database

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 // start of back end
 require('dotenv').config();
-const express = require('express');
 // const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const express = require('express');
 const app = express();
-
 const PORT = 4000;
 
-// prisijungimas prie duomenu bazes
-// mongoose
-//   .connect(process.env.MONGO_CONNECT_STIRNG, {
-//     useFindAndModify: false,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then((result) => {
-//     console.log('Conneced to my Mongoose!');
-//   })
-//   .catch((err) => console.error(err.message));
-
-// const app = express();
+//prisijungimas prie duomenu bazes
+mongoose
+  .connect(process.env.MONGO_CONNECT_STIRNG, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log('Conneced to my Mongoose!');
+  })
+  .catch((err) => console.error(err.message));
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  res.status(200).json(`Serveris veikia an port ${PORT}`);
 });
 
-app.listen(3000);
+app.listen(PORT, console.log(`Back end online on port ${PORT}`));
