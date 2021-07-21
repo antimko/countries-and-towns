84d92a0ecb13d442e279c4486732a0ee5bf193e2
Andrejus Timko <timcostudios@gmail.com>
2021-07-21 10:53:41 +0300

    chaned

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // start of back end
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
